2ca89fa66d418a5048467293ba1eedb2d749b965
sunhk <1853022577@qq.com>
2020-12-01 15:07:53 +0800

    question5,stackoverflow

--- dynamicProgramming/Solution5.java
+++ dynamicProgramming/Solution5.java
@@ -12,6 +12,7 @@
 
 // 来源：力扣（LeetCode）
 // 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 package dynamicProgramming;
 
@@ -25,6 +26,7 @@ class Solution5 {
 	public static void main(String[] args) {
         Solution5 solution5 = new Solution5();
         int [] A={1,2,3};
+        //这个内存太多，超出限制
         System.out.println(solution5.longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth")); 
     }
 
