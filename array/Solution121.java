67f85445f06c0132fc86c420daa6e9d90a1b2474
sunhk <1853022577@qq.com>
2020-12-01 10:44:30 +0800

    34題打卡

---
+++ array/Solution121.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution121 {
+    private static Solution121 solution121;
+	public static void main(String[] args) {
+        solution121 = new Solution121();
+        int [] A={1,2,3};
+        // solution121.largestPerimeter(A);
+    }
+    //34
+    // 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+    // 如果数组中不存在目标值 target，返回 [-1, -1]。
+
+    // 进阶：
+
+    // 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+
+    public int[] searchRange(int[] nums, int target) {
+
+        return null ;
+    }
+   
+
+}
