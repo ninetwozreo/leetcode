974b17a9939499185552773dd595bfbe558f3ae3
sharkme <1853022577@qq.com>
2020-12-08 23:12:58 +0800

    merge+question752

--- bfs/Solution752.java
+++ bfs/Solution752.java
@@ -1,147 +1,71 @@
-// 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+package bfs;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 
-// 示例 1：
+class Solution {
+  public int openLock(String[] deadends, String target) {
+      int step =0;
+      Queue<String> q=new LinkedList<>();
+      Set<String> deads=new HashSet<>();
+      for(String s:deadends){
+          deads.add(s);
+      }
+      q.offer("0000");
 
-// 输入: "babad"
-// 输出: "bab"
-// 注意: "aba" 也是一个有效答案。
-// 示例 2：
+      while (!q.isEmpty()){
+          int cz=q.size();
 
-// 输入: "cbbd"
-// 输出: "bb"
-
-// 来源：力扣（LeetCode）
-// 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
-// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-package dynamicProgramming;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-class Solution5 {
-	public static void main(String[] args) {
-        Solution5 solution5 = new Solution5();
-        int [] A={1,2,3};
-        System.out.println("输出结果为："+solution5.longestPalindrome("babad")); 
-    }
-
-    String res="";
-    //可用 但是超出内存限制
-    public String longestPalindrome(String s) {
-
-        return tranverse(s);
-    }
-
-    String tranverse(String s){
-        if(s.length()<=1){
-            return s;
-        }
-        Map<String,Boolean> valid=new HashMap<>();
-        int lt,rt,rtt;
-        String testS;
-        boolean judge;
-        Boolean hb;
-        for(int i=0;i<s.length();i++){
-          lt=rt=i;
-        //   if(res.length()>)
-          //是否相邻判断
-        //   if(i>0&&i<s.length()-1){
-        //     if(s.charAt(i)==s.charAt(i+1)){
-        //         rt++;
-        //         String testS=s.substring(lt, rt+1);
-                
-        //         res=res.length()>testS.length()?res:testS;
-
-        //     }
-
-        //   }
           
-          rtt=i+1;
-          while(lt>=0&&rt<=s.length()){
-            testS=s.substring(lt, rt);
-               hb=valid.get(testS);
-              if(hb!=null){
-                    judge=hb;
-              }else{
-                judge=isValid(testS);
-                if(judge){
-                    valid.put(testS, judge);
-    
-                   }
+
+          for(int i=0;i<cz;i++){
+              String ns=q.poll();
+              if(deads.contains(ns)){
+                  continue;
               }
-              if(judge){
-
-                res=res.length()>testS.length()?res:testS;
-                
+              if(ns.equals(target)){
+                  return step;
               }
+              for(int j=0;j<=3;j++){
+                  String up=upOne(ns,j);
+                  if(!deads.contains(up)){
+                      q.offer(up);
+                      deads.add(up);
+                  }
+
+                  String down=downOne(ns,j);
               
-
-              if(rtt<=s.length()){
-              
-              testS=s.substring(lt, rtt);
-              hb=valid.get(testS);
-             if(hb!=null){
-                   judge=hb;
-             }else{
-               judge=isValid(testS);
-               if(judge){
-                valid.put(testS, judge);
-
-               }
-             }
-             if(judge){
-
-             res=res.length()>testS.length()?res:testS;
-               
-             }
-            }
-             lt--;
-             rt++;
-             rtt++;
-
+                  if(!deads.contains(down)){
+                      q.offer(down);
+                      deads.add(down);
+                  }
+              }
           }
-        //   lt=rt=i;
-        //   rt+=1;
-        //   while(lt>=0&&rt<=s.length()){
-        //   testS=s.substring(lt, rt);
-        //        hb=valid.get(testS);
-        //       if(hb!=null){
-        //             judge=hb;
-        //       }else{
-        //         judge=isValid(testS);
-        //         valid.put(testS, judge);
-        //       }
-        //       if(judge){
+          step++;
+      }
+      return -1;
+  }
 
-        //       res=res.length()>testS.length()?res:testS;
-                
-        //       }
-        //       lt--;
-        //       rt++;
-        //   }
-        }
-        
-        return res;
-    }
-    boolean isValid(String testS){
-        if(testS.length()<=1){
-            return true;
-        }
-        int lt=0;
-        int rt=testS.length()-1;
-        while(lt<=rt){
-           if(testS.charAt(lt)==testS.charAt(rt)) {
-            lt++;
-            rt--;
-           }else{
-               return false;
-           }
-            
-        }
-        return true;
-        
-    }
+  String upOne(String ns,int index){
+     char[] ar1= ns.toCharArray();
+     if(ar1[index]=='9'){
+         ar1[index]='0';
+     }else{
+         ar1[index]++;
+     }
+     return ar1.toString();
+
+  }
+     String downOne(String ns,int index){
+     char[] ar1= ns.toCharArray();
+     if(ar1[index]=='0'){
+         ar1[index]='9';
+     }else{
+         ar1[index]--;
+     }
+     return ar1.toString();
+
+  }
+
 }
